b22d715fbc877e2bf8d724ae4b0951ddf91905e1
ashishdebnath <blueboyashish2020@gmail.com>
2023-03-14 13:01:34 +0600

    added environment variable

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,52 @@
+import Main from "../../layout/Main";
+import Category from "../../Pages/Category/Category/Category";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login/Login";
+import Register from "../../Pages/Login/Register/Register";
+import News from "../../Pages/News/News/News";
+import Profile from "../../Pages/Others/Profile/Profile";
+import TermsAndCondition from "../../Pages/Others/TermsAndCondition/TermsAndCondition";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>,
+                loader: () => fetch('http://localhost:5000/news')
+            },
+            {
+                path: '/category/:id',
+                element: <Category></Category>,
+                loader: ({ params }) => fetch(`http://localhost:5000/category/${params.id}`)
+            },
+            {
+                path: '/news/:id',
+                element: <PrivateRoute><News></News></PrivateRoute>,
+                loader: ({ params }) => fetch(`http://localhost:5000/news/${params.id}`)
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+            {
+                path: '/terms',
+                element: <TermsAndCondition></TermsAndCondition>
+            },
+            {
+                path: '/profile',
+                element: <PrivateRoute><Profile></Profile></PrivateRoute>
+            }
+
+        ]
+    }
+])
